60286e5d9ab65d2edc978114c6711582654c6c6e
Fran <frangal@gmail.com>
2021-11-28 21:42:08 +0100

    4

--- rpi-img-builder.sh
+++ rpi-img-builder.sh
@@ -163,7 +163,7 @@ systemd-nspawn_exec() {
 
 # Base debootstrap
 COMPONENTS="main contrib non-free"
-MINPKGS="ifupdown openresolv net-tools init dbus rsyslog cron eatmydata wget gnupg"
+MINPKGS="ifupdown openresolv net-tools init dbus rsyslog cron wget gnupg"
 EXTRAPKGS="openssh-server parted locales dosfstools sudo libterm-readline-gnu-perl"
 FIRMWARES="firmware-misc-nonfree firmware-atheros firmware-realtek firmware-libertas firmware-brcm80211"
 WIRELESSPKGS="wpasupplicant crda wireless-tools rfkill wireless-regdb"
@@ -224,31 +224,8 @@ elif [[ "$APT_CACHER" =~ (apt-cacher-ng|root) ]]; then
 fi
 
 status "debootstrap first stage"
-eatmydata debootstrap --foreign --arch="${ARCHITECTURE}" --components="${COMPONENTS// /,}" \
+debootstrap --foreign --arch="${ARCHITECTURE}" --components="${COMPONENTS// /,}" \
   --keyring=$KEYRING --variant - --include="${MINPKGS// /,}" "$RELEASE" "$R" $BOOTSTRAP_URL
-
-for archive in "$R"/var/cache/apt/archives/*eatmydata*.deb; do
-  dpkg-deb --fsys-tarfile "$archive" >"$R"/eatmydata
-  tar -xkf "$R"/eatmydata -C "$R"
-  rm -f "$R"/eatmydata
-done
-
-systemd-nspawn_exec dpkg-divert --divert /usr/bin/dpkg-eatmydata --rename --add /usr/bin/dpkg
-
-cat >"$R"/usr/bin/dpkg <<EOF
-#!/bin/sh
-if [ -e /usr/lib/${LIB_ARCH}/libeatmydata.so ]; then
-    [ -n "\${LD_PRELOAD}" ] && LD_PRELOAD="\$LD_PRELOAD:"
-    LD_PRELOAD="\$LD_PRELOAD\$so"
-fi
-for so in /usr/lib/${LIB_ARCH}/libeatmydata.so; do
-    [ -n "\$LD_PRELOAD" ] && LD_PRELOAD="\$LD_PRELOAD:"
-    LD_PRELOAD="\$LD_PRELOAD\$so"
-done
-export LD_PRELOAD
-exec "\$0-eatmydata" --force-unsafe-io "\$@"
-EOF
-chmod 755 "$R"/usr/bin/dpkg
 
 cat >"$R"/etc/apt/apt.conf.d/99_norecommends <<EOF
 APT::Install-Recommends "false";
@@ -280,7 +257,7 @@ EOF
 fi
 
 status "debootstrap second stage"
-systemd-nspawn_exec eatmydata /debootstrap/debootstrap --second-stage
+systemd-nspawn_exec /debootstrap/debootstrap --second-stage
 
 # Definir sources.list
 if [ "$OS" = "debian" ]; then
@@ -397,8 +374,8 @@ if [[ "${OS}" == "debian" ]]; then
   FIRMWARES="${FIRMWARES}/buster-backports"
 fi
 
-systemd-nspawn_exec eatmydata apt-get update
-systemd-nspawn_exec eatmydata apt-get install -y ${FIRMWARES}
+systemd-nspawn_exec apt-get update
+systemd-nspawn_exec apt-get install -y ${FIRMWARES}
 
 # Disable suspend/resume - speeds up boot massively
 mkdir -p "${R}/etc/initramfs-tools/conf.d/"
@@ -424,7 +401,7 @@ echo "hdmi_force_hotplug=1" >>"$R"/"${BOOT}"/config.txt
 
 status "Instalar paquetes base"
 # shellcheck disable=SC2086
-systemd-nspawn_exec eatmydata apt-get install -y $INCLUDEPKGS
+systemd-nspawn_exec apt-get install -y $INCLUDEPKGS
 status "Activar servicios generate-ssh-host-keys y rpi-resizerootfs"
 #echo | sed -e '/^#/d ; /^ *$/d' | systemd-nspawn_exec <<\EOF
 status "Activar servicio redimendionado partición root"
@@ -592,8 +569,6 @@ if [ -n "$PROXY_URL" ]; then
   unset http_proxy
   rm -rf "$R"/etc/apt/apt.conf.d/66proxy
 fi
-rm -f "$R"/usr/bin/dpkg
-systemd-nspawn_exec dpkg-divert --remove --rename /usr/bin/dpkg
 find "$R"/var/log -depth -type f -print0 | xargs -0 truncate -s 0
 rm -f "$R"/usr/bin/qemu*
 rm -f "$R"/bkp-packages
@@ -692,5 +667,5 @@ fi
 # Tiempo total compilación
 total_time $SECONDS
 # Quit
-log "\n Your image is: $(tput sgr0) $IMGNAME (Size: $(du -h $IMGNAME | cut -f1))"
+log "\n Your image is: \033[0m $IMGNAME (Size: $(du -h $IMGNAME | cut -f1))"
 exit 0
