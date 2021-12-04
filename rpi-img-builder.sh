3d665acfd1899aa22dc49c0262039940659dffa0
Fran <frangal@gmail.com>
2021-12-04 21:41:20 +0100

    ssass

--- rpi-img-builder.sh
+++ rpi-img-builder.sh
@@ -260,18 +260,15 @@ systemd-nspawn_exec /debootstrap/debootstrap --second-stage
 cat "$R"/etc/apt/sources.list
 # Definir sources.list
 if [ "$OS" = "debian" ]; then
-  echo "deb $MIRROR $RELEASE $COMPONENTS" >"$R"/etc/apt/sources.list
   if [ "$RELEASE" = "buster" ]; then
     echo "APT::Default-Release \"$RELEASE\";" >"$R"/etc/apt/apt.conf
-    echo "deb $MIRROR $RELEASE/updates $COMPONENTS" >>"$R"/etc/apt/sources.list
     echo "deb $MIRROR $RELEASE-backports $COMPONENTS" >>"$R"/etc/apt/sources.list
-    echo "deb ${MIRROR}-security/ ${RELEASE}/security $COMPONENTS" >>"$R"/etc/apt/sources.list
+    echo "deb ${MIRROR}-security/ ${RELEASE}/update $COMPONENTS" >>"$R"/etc/apt/sources.list
   elif [ "$RELEASE" == "bullseye" ]; then
     echo "deb $MIRROR $RELEASE-updates $COMPONENTS" >>"$R"/etc/apt/sources.list
     echo "deb ${MIRROR/deb./security.}-security/ ${RELEASE}-security $COMPONENTS" >>"$R"/etc/apt/sources.list
   fi
 elif [ "$OS" = "raspios" ]; then
-  echo "deb $MIRROR $RELEASE $COMPONENTS" >"$R"/etc/apt/sources.list
   if [ "$ARCHITECTURE" = "arm64" ]; then
     echo "deb ${MIRROR_PIOS/raspbian/debian} $RELEASE main" >"$R"/etc/apt/sources.list.d/raspi.list
   elif [ "$ARCHITECTURE" = "armhf" ]; then
